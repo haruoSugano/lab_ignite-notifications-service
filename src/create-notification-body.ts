cd1f2b63d12aa3d7deb8d7a8691715068e3ce596
haruoSugano <haruosugano@icloud.com>
2022-12-12 22:18:44 -0300

    first commit

---
+++ src/create-notification-body.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsUUID, Length } from "class-validator";
+
+export class CreateNotificationBody {
+
+    @IsNotEmpty()
+    @IsUUID()
+    recepientId: string;
+
+    @IsNotEmpty()
+    @Length(5, 240)
+    content: string;
+
+    @IsNotEmpty()
+    category: string;
+}
